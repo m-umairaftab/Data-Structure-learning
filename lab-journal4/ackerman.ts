f1912f61a0bf70ee57627ebd4a5c5eada6d5820a
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-03-27 13:15:22 +0500

    AckerMan with recursive function

---
+++ lab-journal4/ackerman.ts
@@ -0,0 +1,14 @@
+export { }
+function Ackermann(m: number, n: number): any {
+    if (m == 0) {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0) {
+        return Ackermann(m - 1, 1);
+    }
+    else if (m > 0 && n > 0) {
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+}
+
+console.log(Ackermann(2, 2)); 
