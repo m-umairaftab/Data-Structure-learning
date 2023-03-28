213ef62621205a378fd0907a9dbb3ca011902146
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-03-28 09:25:36 +0500

    print Integer In reverve order

--- lab-journal4/print_integer.ts
+++ lab-journal4/print_integer.ts
@@ -1,9 +1,9 @@
 function print(n: number): void {
     if (n < 0) {
-      return; // base case to stop recursion
+        return; // base case to stop recursion
     }
     console.log(n);
     print(n - 1); // recursive call with n-1
-  }
-  
-  print(10);
+}
+
+print(10);
