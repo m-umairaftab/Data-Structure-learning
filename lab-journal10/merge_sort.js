ad6f6aba5c1125e908db625924fbcbfe66e45da5
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-04-13 13:15:47 +0500

    merge Sort

---
+++ lab-journal10/merge_sort.js
@@ -0,0 +1,27 @@
+// export{}
+function mergeSortTopDown(array){
+  if(array.length <= 1){
+    return array
+  }
+  const middle = Math.floor(array.length / 2)
+  const left = array.slice(0, middle)
+  const right = array.slice(middle)
+  return mergeTopDown(mergeSortTopDown(left), mergeSortTopDown(right))
+}
+function mergeTopDown(left, right){
+  const array = []
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      array.push(left.shift())
+    }
+    else{
+      array.push(right.shift())
+    }
+  }
+  return array.concat(left.slice()).concat(right.slice())
+}
+(function test(){
+  const arr = [4,2,1,3,5,6]
+  console.log(mergeSortTopDown(arr));
+  
+})()
