bf0707969bb75c1ea98d9b9f4915a309e235f8f5
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-04-16 00:05:52 +0500

    Quick Sort

---
+++ lab-journal10/quick_sort.js
@@ -0,0 +1,30 @@
+// export{}
+function quickSort(arr) {
+    if (arr.length <= 1) {
+      return arr;
+    }
+    
+    const pivotIndex = Math.floor(arr.length / 2);
+    const pivot = arr[pivotIndex];
+    const left  = [];
+    const right = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+      if (i === pivotIndex) {
+        continue;
+      }
+      
+      if (arr[i] < pivot) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
+    }
+    
+    return quickSort(left).concat([pivot], quickSort(right));
+  }
+  
+  const arr = [4, 2, 1, 3, 5, 6];
+  const sortedArr = quickSort(arr);
+  console.log(sortedArr); // [1, 2, 3, 4, 5, 6]
+  
