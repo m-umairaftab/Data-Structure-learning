d6897f0c6a741170fde2f4fe4c97e348c6c772b2
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-04-17 10:47:25 +0500

    Quick Sort

--- lab-journal10/quick_sort.js
+++ lab-journal10/quick_sort.js
@@ -14,7 +14,7 @@ function quickSort(arr) {
         continue;
       }
       
-      if (arr[i] < pivot) {
+      else if (arr[i] < pivot) {
         left.push(arr[i]);
       } else {
         right.push(arr[i]);
