b49de5c7c3455415cc62ac7865e6935a6e886c9f
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-05-15 15:36:04 +0500

    Serialize and deserialize

---
+++ lab-journal12/serialize.js
@@ -0,0 +1,52 @@
+class TreeNode {
+    constructor(val) {
+      this.val = val;
+      this.left = null;
+      this.right = null;
+    }
+  }
+  
+  function serialize(root) {
+    if (!root) {
+      return 'null';
+    }
+  
+    const leftSerialized = serialize(root.left);
+    const rightSerialized = serialize(root.right);
+  
+    return `${root.val},${leftSerialized},${rightSerialized}`;
+  }
+  
+  function deserialize(data) {
+    const values = data.split(',');
+  
+    function buildTree() {
+      const val = values.shift();
+  
+      if (val === 'null') {
+        return null;
+      }
+  
+      const node = new TreeNode(parseInt(val));
+      node.left = buildTree();
+      node.right = buildTree();
+  
+      return node;
+    }
+  
+    return buildTree();
+  }
+  
+  // Example usage:
+  const root = new TreeNode(1);
+  root.left = new TreeNode(2);
+  root.right = new TreeNode(3);
+  root.right.left = new TreeNode(4);
+  root.right.right = new TreeNode(5);
+  
+  const serialized = serialize(root);
+  console.log(serialized);
+  
+  const deserialized = deserialize(serialized);
+  console.log(deserialized);
+  
