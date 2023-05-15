6e38562b22af08b6ce465041e9627341f004565b
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-05-15 15:08:53 +0500

    Maximum Depth In the tree

--- lab-journal12/maximum_deapth_in_tree.js
+++ lab-journal12/maximum_deapth_in_tree.js
@@ -1,74 +1,46 @@
-// class TreeNode {
-//     constructor(val) {
-//         this.val = val;
-//         this.left = null;
-//         this.right = null;
-//     }
-// }
 
-// function maxDepth(root) {
-//     if (root === null) {
-//         return 0;
-//     }
-
-//     const leftDepth = maxDepth(root.left);
-//     const rightDepth = maxDepth(root.right);
-
-//     return Math.max(leftDepth, rightDepth) + 1;
-// }
-
-// // Example usage:
-// const root = new TreeNode(3);
-// root.left = new TreeNode(9);
-// root.right = new TreeNode(20);
-// root.right.left = new TreeNode(15);
-// root.right.right = new TreeNode(7);
-
-// const depth = maxDepth(root);
-// console.log(depth); // Output: 3
 class TreeNode {
     constructor(val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
+        this.val = val
+        this.left = null
+        this.right = null
     }
 }
 
 function maxDepth(root) {
     if (root === null) {
-        return 0;
+        return 0
     }
 
-    const queue = [root];
-    let depth = 0;
+    const queue = [root]
+    let depth = 0
 
     while (queue.length > 0) {
-        const levelSize = queue.length;
+        const levelSize = queue.length
 
         for (let i = 0; i < levelSize; i++) {
-            const node = queue.shift();
+            const node = queue.shift()
 
             if (node.left !== null) {
-                queue.push(node.left);
+                queue.push(node.left)
             }
 
             if (node.right !== null) {
-                queue.push(node.right);
+                queue.push(node.right)
             }
         }
 
-        depth++;
+        depth++
     }
 
-    return depth;
+    return depth
 }
 
-// Example usage:
-const root = new TreeNode(3);
-root.left = new TreeNode(9);
-root.right = new TreeNode(20);
-root.right.left = new TreeNode(15);
-root.right.right = new TreeNode(7);
+const root = new TreeNode(3)
+root.left = new TreeNode(9)
+root.right = new TreeNode(20)
+root.right.left = new TreeNode(15)
+root.right.right = new TreeNode(7)
 
-const depth = maxDepth(root);
-console.log(depth); // Output: 3
+const depth = maxDepth(root)
+console.log(depth)
