125bb99cec2231f2db30aab0b13b19154c207724
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-05-16 11:07:36 +0500

    Serialize

--- lab-journal12/serialize.js
+++ lab-journal12/serialize.js
@@ -1,52 +1,51 @@
 class TreeNode {
     constructor(val) {
-      this.val = val;
-      this.left = null;
-      this.right = null;
+      this.val = val
+      this.left = null
+      this.right = null
     }
   }
   
   function serialize(root) {
     if (!root) {
-      return 'null';
+      return 'null'
     }
   
-    const leftSerialized = serialize(root.left);
-    const rightSerialized = serialize(root.right);
+    const leftSerialized = serialize(root.left)
+    const rightSerialized = serialize(root.right)
   
-    return `${root.val},${leftSerialized},${rightSerialized}`;
+    return `${root.val},${leftSerialized},${rightSerialized}`
   }
   
   function deserialize(data) {
-    const values = data.split(',');
+    const values = data.split(',')
   
     function buildTree() {
-      const val = values.shift();
+      const val = values.shift()
   
       if (val === 'null') {
-        return null;
+        return null
       }
   
-      const node = new TreeNode(parseInt(val));
-      node.left = buildTree();
-      node.right = buildTree();
+      const node = new TreeNode(parseInt(val))
+      node.left = buildTree()
+      node.right = buildTree()
   
-      return node;
+      return node
     }
   
-    return buildTree();
+    return buildTree()
   }
   
-  // Example usage:
-  const root = new TreeNode(1);
-  root.left = new TreeNode(2);
-  root.right = new TreeNode(3);
-  root.right.left = new TreeNode(4);
-  root.right.right = new TreeNode(5);
+  const root = new TreeNode(1)
+  root.left = new TreeNode(2)
+  root.right = new TreeNode(3)
+  root.right.left = new TreeNode(4)
+  root.right.right = new TreeNode(5)
   
-  const serialized = serialize(root);
-  console.log(serialized);
+  const serialized = serialize(root)
+  console.log(serialized)
   
-  const deserialized = deserialize(serialized);
-  console.log(deserialized);
+  const deserialized = deserialize(serialized)
+  console.log(deserialized)
   
